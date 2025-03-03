ab231e57d3d4611f0764f6de9cb9d5aa0a4aac02
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-02 23:57:47 -0800

    Revert "fix: 2 - RETURN_NULLABLE - return workflowTask; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit b23d7bf818b77ea53bba989194e7f2a86b6cefa3.

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java
@@ -18,7 +18,6 @@ import com.netflix.conductor.common.utils.TaskUtils;
 import com.netflix.conductor.core.execution.WorkflowExecutor;
 import com.netflix.conductor.model.TaskModel;
 import com.netflix.conductor.model.WorkflowModel;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
@@ -71,7 +70,7 @@ public class Join extends WorkflowSystemTask {
       }
 
       // check for optional task failures
-      if (Nullability.castToNonnull(forkedTask.getWorkflowTask(), "reason...").isOptional()
+      if (forkedTask.getWorkflowTask().isOptional()
           && taskStatus == TaskModel.Status.COMPLETED_WITH_ERRORS) {
         optionalTaskFailures
             .append(String.format("%s/%s", forkedTask.getTaskDefName(), forkedTask.getTaskId()))
