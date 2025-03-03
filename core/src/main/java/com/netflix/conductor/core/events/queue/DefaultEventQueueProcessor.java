3842ddf9138d38c98274bc670f0a6cea187be823
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-03 00:03:44 -0800

    Revert "fix: 22 - RETURN_NULLABLE - return taskId; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 2a6fc4254ae5e005ec855d1c2ef03f9c512e9f17.

--- core/src/main/java/com/netflix/conductor/core/events/queue/DefaultEventQueueProcessor.java
+++ core/src/main/java/com/netflix/conductor/core/events/queue/DefaultEventQueueProcessor.java
@@ -25,7 +25,6 @@ import com.netflix.conductor.core.execution.WorkflowExecutor;
 import com.netflix.conductor.model.TaskModel;
 import com.netflix.conductor.model.TaskModel.Status;
 import com.netflix.conductor.model.WorkflowModel;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.*;
 import javax.annotation.Nullable;
 import org.apache.commons.lang3.StringUtils;
@@ -98,9 +97,7 @@ public class DefaultEventQueueProcessor {
                       workflow.getTasks().stream()
                           .filter(
                               task ->
-                                  !task.getStatus().isTerminal()
-                                      && Nullability.castToNonnull(task.getTaskId(), "reason...")
-                                          .equals(taskId))
+                                  !task.getStatus().isTerminal() && task.getTaskId().equals(taskId))
                           .findFirst();
                 } else if (StringUtils.isEmpty(taskRefName)) {
                   LOGGER.error(
