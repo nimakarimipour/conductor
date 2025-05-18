0e6a38f9186cb2654122bb8a558e55832f4bb832
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 15:52:20 -0700

    fix: 22 - RETURN_NULLABLE - return taskId; - returning @Nullable expression from method with @NonNull return type

--- core/src/main/java/com/netflix/conductor/model/TaskModel.java
+++ core/src/main/java/com/netflix/conductor/model/TaskModel.java
@@ -370,6 +370,9 @@ public class TaskModel {
     }
 
     public String getTaskId() {
+        if (taskId == null) {
+            throw new IllegalStateException("taskId cannot be null");
+        }
         return taskId;
     }
 
