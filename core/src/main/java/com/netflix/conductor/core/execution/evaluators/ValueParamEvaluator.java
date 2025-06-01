d3bfd0e33d2ffdd9b325c36c6200d463e0585545
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-31 22:44:35 -0700

    Revert "fix: 11 - DEREFERENCE_NULLABLE - String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass()); - dereferenced expression input is @Nullable"
    This reverts commit 57278d9bd7ba42e4edf865075abc62c5e403ef43.

--- core/src/main/java/com/netflix/conductor/core/execution/evaluators/ValueParamEvaluator.java
+++ core/src/main/java/com/netflix/conductor/core/execution/evaluators/ValueParamEvaluator.java
@@ -14,6 +14,8 @@ package com.netflix.conductor.core.execution.evaluators;
 
 import java.util.Map;
 
+import javax.annotation.Nullable;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -26,20 +28,17 @@ public class ValueParamEvaluator implements Evaluator {
     public static final String NAME = "value-param";
     private static final Logger LOGGER = LoggerFactory.getLogger(ValueParamEvaluator.class);
 
+    @Nullable
     @SuppressWarnings("unchecked")
     @Override
-    public Object evaluate(String expression, Object input) {
+    public Object evaluate(@Nullable String expression, @Nullable Object input) {
         LOGGER.debug("ValueParam evaluator -- evaluating: {}", expression);
         if (input instanceof Map) {
             Object result = ((Map<String, Object>) input).get(expression);
             LOGGER.debug("ValueParam evaluator -- result: {}", result);
             return result;
-        } else if (input != null) {
-            String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass());
-            LOGGER.error(errorMsg);
-            throw new TerminateWorkflowException(errorMsg);
         } else {
-            String errorMsg = "Input is null";
+            String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass());
             LOGGER.error(errorMsg);
             throw new TerminateWorkflowException(errorMsg);
         }
