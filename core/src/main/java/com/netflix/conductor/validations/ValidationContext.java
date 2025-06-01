c44bd4f5e591cf8bfaae30b9929347a206c6f323
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-31 22:34:15 -0700

    fix: 15 - RETURN_NULLABLE - return metadataDAO; - returning @Nullable expression from method with @NonNull return type

--- core/src/main/java/com/netflix/conductor/validations/ValidationContext.java
+++ core/src/main/java/com/netflix/conductor/validations/ValidationContext.java
@@ -29,6 +29,7 @@ public class ValidationContext {
         ValidationContext.metadataDAO = metadataDAO;
     }
 
+    @Nullable
     public static MetadataDAO getMetadataDAO() {
         return metadataDAO;
     }
