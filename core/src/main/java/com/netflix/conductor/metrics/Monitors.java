77bd33c62676dcea63049b665c5c1e27e4f05363
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-31 22:29:11 -0700

    Revert "fix: 8 - FIELD_NO_INIT - private String taskType; - @NonNull field taskType not initialized"
    This reverts commit 9434ee7df608ba7150e1d4dda53755cc1f32e2fa.

--- core/src/main/java/com/netflix/conductor/metrics/Monitors.java
+++ core/src/main/java/com/netflix/conductor/metrics/Monitors.java
@@ -174,7 +174,7 @@ public class Monitors {
         getCounter(classQualifier, name, additionalTags).increment(count);
     }
 
-    public static void recordQueueWaitTime(@Nullable String taskType, long queueWaitTime) {
+    public static void recordQueueWaitTime(String taskType, long queueWaitTime) {
         getTimer(classQualifier, "task_queue_wait", "taskType", taskType)
                 .record(queueWaitTime, TimeUnit.MILLISECONDS);
     }
@@ -283,7 +283,7 @@ public class Monitors {
     }
 
     public static void recordTaskPendingTime(
-            @Nullable String taskType, @Nullable String workflowType, long duration) {
+            String taskType, @Nullable String workflowType, long duration) {
         gauge(
                 classQualifier,
                 "task_pending_time",
@@ -331,7 +331,7 @@ public class Monitors {
     }
 
     public static void recordUpdateConflict(
-            @Nullable String taskType, String workflowType, WorkflowModel.Status status) {
+            String taskType, String workflowType, WorkflowModel.Status status) {
         counter(
                 classQualifier,
                 "task_update_conflict",
@@ -356,7 +356,7 @@ public class Monitors {
                 status.name());
     }
 
-    public static void recordTaskUpdateError(@Nullable String taskType, String workflowType) {
+    public static void recordTaskUpdateError(String taskType, String workflowType) {
         counter(
                 classQualifier,
                 "task_update_error",
@@ -366,8 +366,7 @@ public class Monitors {
                 taskType);
     }
 
-    public static void recordTaskExtendLeaseError(
-            @Nullable String taskType, @Nullable String workflowType) {
+    public static void recordTaskExtendLeaseError(String taskType, @Nullable String workflowType) {
         counter(
                 classQualifier,
                 "task_extendLease_error",
@@ -377,7 +376,7 @@ public class Monitors {
                 taskType);
     }
 
-    public static void recordTaskQueueOpError(@Nullable String taskType, String workflowType) {
+    public static void recordTaskQueueOpError(String taskType, String workflowType) {
         counter(
                 classQualifier,
                 "task_queue_op_error",
@@ -475,8 +474,7 @@ public class Monitors {
                 exceptionClazz);
     }
 
-    public static void recordEventActionError(
-            String action, @Nullable String entityName, String event) {
+    public static void recordEventActionError(String action, String entityName, String event) {
         counter(
                 classQualifier,
                 "event_action_error",
