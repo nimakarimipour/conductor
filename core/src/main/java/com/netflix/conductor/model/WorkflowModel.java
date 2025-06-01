390bb40775e5402bd42474cdbbd8d902f7c7420e
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-31 22:44:49 -0700

    fix: 14 - FIELD_NO_INIT - private WorkflowDef workflowDefinition; - @NonNull field workflowDefinition not initialized

--- core/src/main/java/com/netflix/conductor/model/WorkflowModel.java
+++ core/src/main/java/com/netflix/conductor/model/WorkflowModel.java
@@ -83,7 +83,7 @@ public class WorkflowModel {
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Set<String> failedTaskNames = new HashSet<>();
 
-    private WorkflowDef workflowDefinition;
+    private WorkflowDef workflowDefinition = new WorkflowDef();
 
     @Nullable private String externalInputPayloadStoragePath;
 
