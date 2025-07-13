c4eb0174688088817b0295ea82e5afe7577ab04b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:18:09 -0700

    Revert "fix: 11 - DEREFERENCE_NULLABLE - String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass()); - dereferenced expression input is @Nullable"
    This reverts commit 0f05970acc1abf07a3444e498e5792fac670d669.

--- core/src/main/java/com/netflix/conductor/core/execution/evaluators/ValueParamEvaluator.java
+++ core/src/main/java/com/netflix/conductor/core/execution/evaluators/ValueParamEvaluator.java
@@ -37,12 +37,8 @@ public class ValueParamEvaluator implements Evaluator {
             Object result = ((Map<String, Object>) input).get(expression);
             LOGGER.debug("ValueParam evaluator -- result: {}", result);
             return result;
-        } else if (input != null) {
-            String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass());
-            LOGGER.error(errorMsg);
-            throw new TerminateWorkflowException(errorMsg);
         } else {
-            String errorMsg = "Input is null";
+            String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass());
             LOGGER.error(errorMsg);
             throw new TerminateWorkflowException(errorMsg);
         }
