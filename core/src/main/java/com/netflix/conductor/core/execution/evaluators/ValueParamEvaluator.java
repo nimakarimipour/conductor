a596816b146cd9f3e22037ee51238ae051df36fc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 00:07:42 -0700

    before:19to18 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression input is @Nullable' /home/nima/Developer/nullness-benchmarks/conductor/core/src/main/java/com/netflix/conductor/core/execution/evaluators/ValueParamEvaluator.java:41             String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass());

--- core/src/main/java/com/netflix/conductor/core/execution/evaluators/ValueParamEvaluator.java
+++ core/src/main/java/com/netflix/conductor/core/execution/evaluators/ValueParamEvaluator.java
@@ -28,19 +28,22 @@ public class ValueParamEvaluator implements Evaluator {
     public static final String NAME = "value-param";
     private static final Logger LOGGER = LoggerFactory.getLogger(ValueParamEvaluator.class);
 
-    @Nullable
-    @SuppressWarnings("unchecked")
-    @Override
-    public Object evaluate(@Nullable String expression, @Nullable Object input) {
-        LOGGER.debug("ValueParam evaluator -- evaluating: {}", expression);
-        if (input instanceof Map) {
-            Object result = ((Map<String, Object>) input).get(expression);
-            LOGGER.debug("ValueParam evaluator -- result: {}", result);
-            return result;
-        } else {
-            String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass());
-            LOGGER.error(errorMsg);
-            throw new TerminateWorkflowException(errorMsg);
-        }
-    }
+    @Nullable @SuppressWarnings("unchecked")
+      @Override
+      public Object evaluate(@Nullable String expression, @Nullable Object input) {
+          LOGGER.debug("ValueParam evaluator -- evaluating: {}", expression);
+          if (input instanceof Map) {
+              Object result = ((Map<String, Object>) input).get(expression);
+              LOGGER.debug("ValueParam evaluator -- result: {}", result);
+              return result;
+          } else if (input != null) {
+              String errorMsg = String.format("Input has to be a JSON object: %s", input.getClass());
+              LOGGER.error(errorMsg);
+              throw new TerminateWorkflowException(errorMsg);
+          } else {
+              String errorMsg = "Input is null";
+              LOGGER.error(errorMsg);
+              throw new TerminateWorkflowException(errorMsg);
+          }
+      }
 }
