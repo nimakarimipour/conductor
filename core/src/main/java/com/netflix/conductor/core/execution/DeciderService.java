5ca3c77680b95e39fc8ac0bc242be62989f81043
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:20:06 -0700

    fix: 25 - DEREFERENCE_NULLABLE - return taskMappers - dereferenced expression taskMappers                 .getOrDefault(type, taskMappers.get(USER_DEFINED.name())) is @Nullable

--- core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
+++ core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
@@ -837,7 +837,6 @@ public class DeciderService {
 
         String type = taskToSchedule.getType();
 
-        // get tasks already scheduled (in progress/terminal) for  this workflow instance
         List<String> tasksInWorkflow =
                 workflow.getTasks().stream()
                         .filter(
@@ -860,15 +859,13 @@ public class DeciderService {
                         .withDeciderService(this)
                         .build();
 
-        // For static forks, each branch of the fork creates a join task upon completion for
-        // dynamic forks, a join task is created with the fork and also with each branch of the
-        // fork.
-        // A new task must only be scheduled if a task, with the same reference name is not already
-        // in this workflow instance
-        return taskMappers
-                .getOrDefault(type, taskMappers.get(USER_DEFINED.name()))
-                .getMappedTasks(taskMapperContext)
-                .stream()
+        TaskMapper taskMapper =
+                taskMappers.getOrDefault(type, taskMappers.get(USER_DEFINED.name()));
+        if (taskMapper == null) {
+            throw new IllegalStateException("TaskMapper not found for type: " + type);
+        }
+
+        return taskMapper.getMappedTasks(taskMapperContext).stream()
                 .filter(task -> !tasksInWorkflow.contains(task.getReferenceTaskName()))
                 .collect(Collectors.toList());
     }
