31f395dcc3f920197d4fa9d7b8b2bf440a8697ff
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 00:08:11 -0700

    before:15to14 - Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/nima/Developer/nullness-benchmarks/conductor/core/src/main/java/com/netflix/conductor/validations/ValidationContext.java:33         return metadataDAO;

--- core/src/main/java/com/netflix/conductor/validations/ValidationContext.java
+++ core/src/main/java/com/netflix/conductor/validations/ValidationContext.java
@@ -30,6 +30,9 @@ public class ValidationContext {
     }
 
     public static MetadataDAO getMetadataDAO() {
-        return metadataDAO;
-    }
+          if (metadataDAO == null) {
+              throw new IllegalStateException("MetadataDAO has not been initialized");
+          }
+          return metadataDAO;
+      }
 }
