add17c45d28503499294803bbab2b483866e26aa
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:18:01 -0700

    Revert "fix: 10 - FIELD_NO_INIT - private Status status; - @NonNull field status not initialized"
    This reverts commit 375c519526863b1cc1d94a984e0da597ae2ff583.

--- core/src/main/java/com/netflix/conductor/model/TaskModel.java
+++ core/src/main/java/com/netflix/conductor/model/TaskModel.java
@@ -70,7 +70,7 @@ public class TaskModel {
 
     private String taskType;
 
-    private Status status = new Status(); // Initialize with a default Status object
+    private Status status;
 
     @Nullable private String referenceTaskName;
 
