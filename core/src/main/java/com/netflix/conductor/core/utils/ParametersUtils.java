aa0c8d65562dc7f56ba86977f3da6337b44578f2
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:18:56 -0700

    fix: 17 - DEREFERENCE_NULLABLE - String[] values = paramString.split("(?=(?<!\\$)\\$\\{)|(?<=})"); - dereferenced expression paramString is @Nullable

--- core/src/main/java/com/netflix/conductor/core/utils/ParametersUtils.java
+++ core/src/main/java/com/netflix/conductor/core/utils/ParametersUtils.java
@@ -225,14 +225,16 @@ public class ParametersUtils {
             @Nullable String paramString,
             DocumentContext documentContext,
             @Nullable String taskId) {
+        if (paramString == null) {
+            return null;
+        }
+
         String[] values = paramString.split("(?=(?<!\\$)\\$\\{)|(?<=})");
         Object[] convertedValues = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
             convertedValues[i] = values[i];
             if (values[i].startsWith("${") && values[i].endsWith("}")) {
                 String paramPath = values[i].substring(2, values[i].length() - 1);
-                // if the paramPath is blank, meaning no value in between ${ and }
-                // like ${}, ${  } etc, set the value to empty string
                 if (StringUtils.isBlank(paramPath)) {
                     convertedValues[i] = "";
                     continue;
@@ -260,7 +262,6 @@ public class ParametersUtils {
         }
 
         Object retObj = convertedValues[0];
-        // If the parameter String was "v1 v2 v3" then make sure to stitch it back
         if (convertedValues.length > 1) {
             for (int i = 0; i < convertedValues.length; i++) {
                 Object val = convertedValues[i];
