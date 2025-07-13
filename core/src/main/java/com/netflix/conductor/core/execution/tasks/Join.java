d69ecf8cc7c27bf2015a016220e9eb2841ace5ab
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:18:24 -0700

    fix: 13 - DEREFERENCE_NULLABLE - joinOn.stream() - dereferenced expression joinOn is @Nullable

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java
@@ -41,8 +41,10 @@ public class Join extends WorkflowSystemTask {
         StringBuilder failureReason = new StringBuilder();
         StringBuilder optionalTaskFailures = new StringBuilder();
         List<String> joinOn = (List<String>) task.getInputData().get("joinOn");
+        if (joinOn == null) {
+            return false;
+        }
         if (task.isLoopOverTask()) {
-            // If join is part of loop over task, wait for specific iteration to get complete
             joinOn =
                     joinOn.stream()
                             .map(name -> TaskUtils.appendIteration(name, task.getIteration()))
@@ -51,7 +53,6 @@ public class Join extends WorkflowSystemTask {
         for (String joinOnRef : joinOn) {
             TaskModel forkedTask = workflow.getTaskByRefName(joinOnRef);
             if (forkedTask == null) {
-                // Task is not even scheduled yet
                 allDone = false;
                 break;
             }
@@ -60,7 +61,6 @@ public class Join extends WorkflowSystemTask {
             if (hasFailures) {
                 failureReason.append(forkedTask.getReasonForIncompletion()).append(" ");
             }
-            // Only add to task output if it's not empty
             if (!forkedTask.getOutputData().isEmpty()) {
                 task.addOutput(joinOnRef, forkedTask.getOutputData());
             }
@@ -71,7 +71,6 @@ public class Join extends WorkflowSystemTask {
                 break;
             }
 
-            // check for optional task failures
             if (forkedTask.getWorkflowTask().isOptional()
                     && taskStatus == TaskModel.Status.COMPLETED_WITH_ERRORS) {
                 optionalTaskFailures
