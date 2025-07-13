f2b04d21ebc79442010cc758e6f6c290ec07ca19
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 00:07:49 -0700

    before:18to16 - Type='DEREFERENCE_NULLABLE', message='enhanced-for expression joinOn is @Nullable' /home/nima/Developer/nullness-benchmarks/conductor/core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java:51         for (String joinOnRef : joinOn) {

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java
@@ -32,71 +32,76 @@ public class Join extends WorkflowSystemTask {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
-    public boolean execute(
-            WorkflowModel workflow, TaskModel task, WorkflowExecutor workflowExecutor) {
-
-        boolean allDone = true;
-        boolean hasFailures = false;
-        StringBuilder failureReason = new StringBuilder();
-        StringBuilder optionalTaskFailures = new StringBuilder();
-        List<String> joinOn = (List<String>) task.getInputData().get("joinOn");
-        if (task.isLoopOverTask()) {
-            // If join is part of loop over task, wait for specific iteration to get complete
-            joinOn =
-                    joinOn.stream()
-                            .map(name -> TaskUtils.appendIteration(name, task.getIteration()))
-                            .collect(Collectors.toList());
-        }
-        for (String joinOnRef : joinOn) {
-            TaskModel forkedTask = workflow.getTaskByRefName(joinOnRef);
-            if (forkedTask == null) {
-                // Task is not even scheduled yet
-                allDone = false;
-                break;
-            }
-            TaskModel.Status taskStatus = forkedTask.getStatus();
-            hasFailures = !taskStatus.isSuccessful() && !forkedTask.getWorkflowTask().isOptional();
-            if (hasFailures) {
-                failureReason.append(forkedTask.getReasonForIncompletion()).append(" ");
-            }
-            // Only add to task output if it's not empty
-            if (!forkedTask.getOutputData().isEmpty()) {
-                task.addOutput(joinOnRef, forkedTask.getOutputData());
-            }
-            if (!taskStatus.isTerminal()) {
-                allDone = false;
-            }
-            if (hasFailures) {
-                break;
-            }
-
-            // check for optional task failures
-            if (forkedTask.getWorkflowTask().isOptional()
-                    && taskStatus == TaskModel.Status.COMPLETED_WITH_ERRORS) {
-                optionalTaskFailures
-                        .append(
-                                String.format(
-                                        "%s/%s",
-                                        forkedTask.getTaskDefName(), forkedTask.getTaskId()))
-                        .append(" ");
-            }
-        }
-        if (allDone || hasFailures || optionalTaskFailures.length() > 0) {
-            if (hasFailures) {
-                task.setReasonForIncompletion(failureReason.toString());
-                task.setStatus(TaskModel.Status.FAILED);
-            } else if (optionalTaskFailures.length() > 0) {
-                task.setStatus(TaskModel.Status.COMPLETED_WITH_ERRORS);
-                optionalTaskFailures.append("completed with errors");
-                task.setReasonForIncompletion(optionalTaskFailures.toString());
-            } else {
-                task.setStatus(TaskModel.Status.COMPLETED);
-            }
-            return true;
-        }
-        return false;
-    }
+      @SuppressWarnings("unchecked")
+      public boolean execute(
+              WorkflowModel workflow, TaskModel task, WorkflowExecutor workflowExecutor) {
+    
+          boolean allDone = true;
+          boolean hasFailures = false;
+          StringBuilder failureReason = new StringBuilder();
+          StringBuilder optionalTaskFailures = new StringBuilder();
+          List<String> joinOn = (List<String>) task.getInputData().get("joinOn");
+    
+          if (joinOn == null) {
+              return false; // or handle appropriately
+          }
+    
+          if (task.isLoopOverTask()) {
+              // If join is part of loop over task, wait for specific iteration to get complete
+              joinOn =
+                      joinOn.stream()
+                              .map(name -> TaskUtils.appendIteration(name, task.getIteration()))
+                              .collect(Collectors.toList());
+          }
+          for (String joinOnRef : joinOn) {
+              TaskModel forkedTask = workflow.getTaskByRefName(joinOnRef);
+              if (forkedTask == null) {
+                  // Task is not even scheduled yet
+                  allDone = false;
+                  break;
+              }
+              TaskModel.Status taskStatus = forkedTask.getStatus();
+              hasFailures = !taskStatus.isSuccessful() && !forkedTask.getWorkflowTask().isOptional();
+              if (hasFailures) {
+                  failureReason.append(forkedTask.getReasonForIncompletion()).append(" ");
+              }
+              // Only add to task output if it's not empty
+              if (!forkedTask.getOutputData().isEmpty()) {
+                  task.addOutput(joinOnRef, forkedTask.getOutputData());
+              }
+              if (!taskStatus.isTerminal()) {
+                  allDone = false;
+              }
+              if (hasFailures) {
+                  break;
+              }
+    
+              // check for optional task failures
+              if (forkedTask.getWorkflowTask().isOptional()
+                      && taskStatus == TaskModel.Status.COMPLETED_WITH_ERRORS) {
+                  optionalTaskFailures
+                          .append(
+                                  String.format(
+                                          "%s/%s",
+                                          forkedTask.getTaskDefName(), forkedTask.getTaskId()))
+                          .append(" ");
+              }
+          }
+          if (allDone || hasFailures || optionalTaskFailures.length() > 0) {
+              if (hasFailures) {
+                  task.setReasonForIncompletion(failureReason.toString());
+                  task.setStatus(TaskModel.Status.FAILED);
+              } else if (optionalTaskFailures.length() > 0) {
+                  task.setStatus(TaskModel.Status.COMPLETED_WITH_ERRORS);
+                  optionalTaskFailures.append("completed with errors");
+                  task.setReasonForIncompletion(optionalTaskFailures.toString());
+              } else {
+                  task.setStatus(TaskModel.Status.COMPLETED);
+              }
+              return true;
+          }
+          return false;
+      }
 
     public boolean isAsync() {
         return true;
